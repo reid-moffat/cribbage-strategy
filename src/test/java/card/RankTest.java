23e9fdae2b0a5e174ff7b0ebf5d5c4da3c5ecc22
Reid Moffat <18rem8@queensu.ca>
2021-11-17 17:39:49 -0500

    Rank unit tests

--- src/test/java/card/RankTest.java
+++ src/test/java/card/RankTest.java
@@ -1,75 +1,55 @@
 package card;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class RankTest {
 
-    @BeforeEach
-    void setUp() {
-        assertEquals(1, 1);
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void name() {
-    }
-
-    @Test
-    void ordinal() {
-    }
+    private final Rank[] ranks = {Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX,
+            Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING};
 
     @Test
     void testToString() {
+        final String[] names = {"ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+                "NINE", "TEN", "JACK", "QUEEN", "KING"};
+        for (int i = 0; i < ranks.length; ++i) {
+            assertEquals(ranks[i].toString(), names[i]);
+        }
     }
 
     @Test
     void testEquals() {
+        Rank r1 = Rank.FOUR;
+        Rank r2 = Rank.FOUR;
+        Rank r3 = Rank.FIVE;
+
+        assertEquals(r1, r2);
+        assertNotEquals(r1, r3);
+        assertNotEquals(r2, r3);
     }
 
     @Test
     void testHashCode() {
-    }
+        // Test that the hashCode value for a specific rank is constant
+        // no matter when it is calculated or what variable it is from
+        int hashCode1, hashCode2;
+        for (Rank r : ranks) {
+            hashCode1 = r.hashCode();
+            Rank r2 = r;
+            hashCode2 = r.hashCode();
 
-    @Test
-    void testClone() {
-    }
-
-    @Test
-    void compareTo() {
-    }
-
-    @Test
-    void getDeclaringClass() {
-    }
-
-    @Test
-    void describeConstable() {
-    }
-
-    @Test
-    void valueOf() {
-    }
-
-    @Test
-    void testFinalize() {
+            assertEquals(hashCode1, hashCode2);
+            assertEquals(hashCode1, r2.hashCode());
+        }
     }
 
     @Test
     void getRankValue() {
+        for (int i = 0; i < ranks.length; ++i) {
+            assertEquals(ranks[i].getRankValue(), i + 1);
+        }
     }
 
-    @Test
-    void values() {
-    }
-
-    @Test
-    void testValueOf() {
-    }
 }
