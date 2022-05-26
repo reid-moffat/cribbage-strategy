428fa40e322be4bce6a69e1bf09a234c6cdeb8e9
Reid Moffat <18rem8@queensu.ca>
2022-05-25 22:25:00 -0400

    ui pile check

--- src/test/java/main/UserInterfaceTest.java
+++ src/test/java/main/UserInterfaceTest.java
@@ -1,8 +1,23 @@
 package main;
 
+import card.Card;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class UserInterfaceTest {
+
+    private final UserInterface ui = new UserInterface();
+
+    @Test
+    void dataIntegrity() throws NoSuchFieldException, IllegalAccessException {
+        final Field cardPile = ui.getClass().getDeclaredField("cardPile");
+        cardPile.setAccessible(true);
+        assertEquals(((HashSet<Card>) cardPile.get(ui)).size(), 52);
+    }
 
     @Test
     @SuppressWarnings({"UnusedDeclaration"})
